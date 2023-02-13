9ff9c4ab7c3779501abc903c61a0cef9afc7be46
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2023-02-13 22:27:04 +0000

    Logbook 2

---
+++ Logbook/Week 2/wk2ex9.php
@@ -0,0 +1,12 @@
+<?
+  $topmodules[0] = "Internet Systems Development";
+  $topmodules[5] = "Programming 1";
+  $topmodules[10] = "Programming 2";
+  $topmodules[30] = "OOAD";
+  $topmodules[40] = "Software Engineering";
+
+  for($count = 0,$count < 5;$count++;)
+  {
+    echo "Index is $index and value is $value <br/>";
+  }
+?>
