ff8d86319d4d3de5f4ed62a6fe62822c61f1c9d1
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2023-02-18 17:09:58 +0000

    Added Week 2 Exercises

---
+++ Logbook/Week 2/wk2ex10.php
@@ -0,0 +1,11 @@
+<?
+  $mymarks["year 1"] = 55;
+  $mymarks["year 2"] = 65;
+  $mymarks["year 3"] = 75;
+
+  for($count = 0,$count < 5;$count++;)
+  {
+    echo "for  $index  my grade was  $value <br/>";
+  }
+  echo "<br/> My best year was Year 3 when I averaged ". $mymarks[“year 3"];
+?>
---
+++ Logbook/Week 2/wk2ex8.php
@@ -0,0 +1,12 @@
+<?php
+  $topModules[0] = "Internet Systems Development";
+  $topModules[1] = "Programming 1";
+  $topModules[2] = "Programming 2";
+  $topModules[3] = "OOAD";
+  $topModules[4] = "Software Engineering";
+
+  for($count = 0,$count < 5;$count++;)
+  {
+    echo "$count module is $topModules[$count] <br/>";
+  }	
+?>
