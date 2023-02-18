5a5bdb71b5313b56bba8ff2c982d43f6f75aaf29
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2023-02-18 17:11:44 +0000

    Week 2 Exercises

--- Logbook/Week 2/wk2ex9.php
+++ Logbook/Week 2/wk2ex9.php
@@ -1,11 +1,11 @@
-<?
-  $topmodules[0] = "Internet Systems Development";
+<?php
+  $topmodules[0] = "Open Source Systems";
   $topmodules[5] = "Programming 1";
   $topmodules[10] = "Programming 2";
-  $topmodules[30] = "OOAD";
+  $topmodules[30] = "OOSD";
   $topmodules[40] = "Software Engineering";
 
-  for($count = 0,$count < 5;$count++;)
+  foreach ($topmodules as $index => $value)
   {
     echo "Index is $index and value is $value <br/>";
   }
